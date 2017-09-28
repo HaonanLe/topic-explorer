7999fec79202cebf7179b51627568bee13617483
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-09-28 17:27:51 -0400

    updating coverage

--- coverage.sh
+++ coverage.sh
@@ -12,9 +12,26 @@ EXIT=$(($EXIT+$?))
 
 # Special thanks on the `trap` semantics to:
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
-#disown -h $CMD -m topicexplorer.server -p 8000 --no-browser ap.ini &
-#DEMO_PID=$!
-#EXIT=$(($EXIT+$?))
+set -m # Reactivate monitor mode!
+$CMD -m topicexplorer.server -p 8000 --no-browser ap.ini &
+DEMO_PID=$!
+
+trap 'echo "trying to kill $DEMO_PID" && kill -2 $DEMO_PID && echo "killed $DEMO_PID" && wait $DEMO_PID && echo "waited for $DEMO_PID";' INT
+sleep 10
+
+[ $(curl -i http://localhost:8000/docs.json 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+EXIT=$(($EXIT+$?))
+[ $(curl -i http://localhost:8000/20/topics.json 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+EXIT=$(($EXIT+$?))
+[ $(curl -i http://localhost:8000/ 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+EXIT=$(($EXIT+$?))
+[ $(curl -i http://localhost:8000/20/ 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+EXIT=$(($EXIT+$?))
+[ $(curl -i http://localhost:8000/topics 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+EXIT=$(($EXIT+$?))
+
+kill -2 $$
+trap - INT
 
 $CMD -m topicexplorer.train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
 EXIT=$(($EXIT+$?))
